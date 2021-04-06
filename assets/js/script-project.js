c36fb11e8a4654ff92439dce82b493593a9b47d9
Jheymison Simões <jheymisonbao@live.com>
2021-04-06 20:01:14 -0300

    Portifolio para apresentação dos projetos

---
+++ assets/js/script-project.js
@@ -0,0 +1,28 @@
+let dadosProject = [{
+        img: "../images/projetos/vaccination.PNG",
+        title: "Controle de Vacinação",
+        description: "Pequeno projeto realizado como desafio para o controle de de vacinação.",
+        tech: "JAVA - Spring Boot - React - MySQL",
+        linkPreview: "https://vaccinationcontrol.netlify.app/",
+        linkGitBack: "https://github.com/jheymison-simoes/controlevacina-back-end",
+        linkGitFront: "https://github.com/jheymison-simoes/controlevacina-front-end"
+    },
+    {
+        img: "../images/projetos/vaccination.PNG",
+        title: "Ecommerce Petshop",
+        description: "Projeto feito realizado como defesa de TCC com integração com Whatsapp",
+        tech: "Reactjs - Nodejs - MySQL - Javascript",
+        linkPreview: "https://projectpetshop.netlify.app/",
+        linkGitBack: "https://github.com/jheymison-simoes/backend_petshop",
+        linkGitFront: "https://github.com/jheymison-simoes/testeprojectpetshop"
+    },
+    {
+        img: "../images/projetos/vaccination.PNG",
+        title: "Ecommerce Petshop",
+        description: "Projeto feito realizado como defesa de TCC com integração com Whatsapp",
+        tech: "Reactjs - Nodejs - MySQL - Javascript",
+        linkPreview: "https://projectpetshop.netlify.app/",
+        linkGitBack: "https://github.com/jheymison-simoes/backend_petshop",
+        linkGitFront: "https://github.com/jheymison-simoes/testeprojectpetshop"
+    }
+]
