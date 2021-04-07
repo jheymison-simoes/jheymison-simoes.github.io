268a69e12652327b0b772a35a3b98e76cdf1574c
Jheymison Simões <jheymisonbao@live.com>
2021-04-06 22:02:01 -0300

    Adicionando menu responsivo

--- assets/js/script-project.js
+++ assets/js/script-project.js
@@ -1,28 +1,12 @@
-let dadosProject = [{
-        img: "../images/projetos/vaccination.PNG",
-        title: "Controle de Vacinação",
-        description: "Pequeno projeto realizado como desafio para o controle de de vacinação.",
-        tech: "JAVA - Spring Boot - React - MySQL",
-        linkPreview: "https://vaccinationcontrol.netlify.app/",
-        linkGitBack: "https://github.com/jheymison-simoes/controlevacina-back-end",
-        linkGitFront: "https://github.com/jheymison-simoes/controlevacina-front-end"
-    },
-    {
-        img: "../images/projetos/vaccination.PNG",
-        title: "Ecommerce Petshop",
-        description: "Projeto feito realizado como defesa de TCC com integração com Whatsapp",
-        tech: "Reactjs - Nodejs - MySQL - Javascript",
-        linkPreview: "https://projectpetshop.netlify.app/",
-        linkGitBack: "https://github.com/jheymison-simoes/backend_petshop",
-        linkGitFront: "https://github.com/jheymison-simoes/testeprojectpetshop"
-    },
-    {
-        img: "../images/projetos/vaccination.PNG",
-        title: "Ecommerce Petshop",
-        description: "Projeto feito realizado como defesa de TCC com integração com Whatsapp",
-        tech: "Reactjs - Nodejs - MySQL - Javascript",
-        linkPreview: "https://projectpetshop.netlify.app/",
-        linkGitBack: "https://github.com/jheymison-simoes/backend_petshop",
-        linkGitFront: "https://github.com/jheymison-simoes/testeprojectpetshop"
+function openCloseMenu() {
+    var divMenuResposivo = document.getElementById("menu-responsive").style.top;
+
+    console.log(divMenuResposivo);
+
+    if (divMenuResposivo == "-170px") {
+        document.getElementById("menu-responsive").style.top = "0px";
+    } else {
+        document.getElementById("menu-responsive").style.top = "-170px";
     }
-]
+
+}
